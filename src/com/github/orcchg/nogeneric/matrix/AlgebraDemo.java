8e3d931effeb21157b2f7935cf4441a5e7b42b2e
Alov Maxim <alovmax@yandex.ru>
2014-06-22 00:02:10 +0400

    Todo: presice double compare

--- src/com/github/orcchg/nogeneric/matrix/AlgebraDemo.java
+++ src/com/github/orcchg/nogeneric/matrix/AlgebraDemo.java
@@ -5,8 +5,12 @@ import com.github.orcchg.nogeneric.matrix.SquareMatrix;
 public class AlgebraDemo {
 
   public static void main(String[] args) {
-    SquareMatrix mat1 = new SquareMatrix(new double[][] {{1, 2}, {3, 4}});
+    SquareMatrix mat1 = new SquareMatrix(new double[][] {{1, 2, 3}, {4, 5, 6}, {2, 5, 3}});
     SquareMatrix identity = new SquareMatrix(new double[][] {{1, 0}, {0, 1}});
+    
+    System.out.println(mat1.determinant());
+    System.out.println(identity.determinant());
+    System.out.println("\n");
     
     System.out.println(mat1.toString());
     System.out.println(identity.toString());
@@ -20,6 +24,7 @@ public class AlgebraDemo {
     System.out.println(mat1.getInversed().mul(mat1));
     System.out.println(identity.getInversed().mul(identity));
     System.out.println("Inversion: " + mat1.getInversed().mul(mat1).equals(identity));
+    System.out.println("Inverted:\n" + mat1.getInversed().mul(mat1));
     
     System.out.println(mat1.determinant());
     System.out.println(identity.determinant());
